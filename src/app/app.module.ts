4d6f0d47628e2ba126ebfb696bc4066ea82d83d9
Viviana Benitez <magi61012@gmail.com>
2020-05-07 09:35:36 +0100

    done HU-02

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,32 @@ import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
 
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAnalyticsModule,
-    AngularFirestoreModule
-
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [
   
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit, NgZone } from '@angular/core';
+import { FormBuilder, Validators,FormControl} from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+
+@Component({
+ selector: 'app-login',
+ templateUrl: './login.component.html',
+ styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent implements OnInit {
+ errorMessage = '';
+ hide = true;
+ email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+ constructor(private afAuth: AngularFireAuth,
+   private router: Router,
+   private fb: FormBuilder,
+   private ngZone: NgZone) { }
+
+ loginForm = this.fb.group({
+   username: ['', Validators.required],
+   password: ['', Validators.required]
+ })
+
+ ngOnInit() {
+  this.afAuth.user.subscribe(user => {
+    if (user) {
+      this.ngZone.run(() => {
+        this.router.navigate(['/inicio']);
+    })
+}})
+}
+
+createUser() {
+  this.afAuth.createUserWithEmailAndPassword(this.loginForm.value.username, this.loginForm.value.password).then(() => {
+    this.router.navigate(['/inicio']);
+  }).catch(response => {
+    this.errorMessage = response.message;
+});
+}
+
+signIn() { 
+  console.log("hice click"); 
+  this.afAuth.signInWithEmailAndPassword(this.loginForm.value.username, this.loginForm.value.password).then(() => {
+    this.router.navigate(['/inicio']);
+   }).catch(response => {
+     this.errorMessage = response.message;
+     console.log("ERROR MSG: ", this.errorMessage)
+    });
+}
+
+}
