bb996113a0f225f42a86eecfac44211402785099
Cristina <cristinaquinsan710@gmail.com>
2020-05-11 12:30:06 +0100

    expenses and income done, doing css

--- src/app/components/frontexpenses/frontexpenses.component.ts
+++ src/app/components/frontexpenses/frontexpenses.component.ts
@@ -2,7 +2,9 @@ import  {Component} from '@angular/core';
 
 @Component({
     selector: 'app-frontexpenses',
-    templateUrl: './frontexpenses.component.html'
+    templateUrl: './frontexpenses.component.html',
+    styleUrls: ['./frontexpenses.component.css'],
+
 })
 export class expensesComponent{
 
