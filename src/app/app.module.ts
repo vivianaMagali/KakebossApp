d0c29dde5575f81927f8dc2c986bff907389ba3c
Cristina <cristinaquinsan710@gmail.com>
2020-05-11 17:33:22 +0100

    finally css worked as expected

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 
 
@@ -34,8 +35,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatDividerModule,
     MatListModule,
     MatCardModule,
-    MatButtonModule
-
+    MatButtonModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
