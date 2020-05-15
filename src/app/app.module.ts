dd3aed0504a42fd82a7674b585368244a8b6f0e8
Cristina <cristinaquinsan710@gmail.com>
2020-05-15 12:07:12 +0100

    modules ordered

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,27 @@
+/** BASIC MODULES */
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { Routes } from '@angular/router';
+
+/** FIREBASE MODULES */
 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { config } from './services/utils/firebase';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { config } from './services/utils/firebase';
 
+/** ANGULAR MATERIAL MODULES */
+
+import { MatFormFieldModule, MatFormField } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { ExpensesComponent } from './components/expenses/expenses.component';
-
-import { MatFormFieldModule, MatFormField } from '@angular/material/form-field';
-import { MatRadioModule } from '@angular/material/radio';
-
-
-
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
@@ -28,12 +29,16 @@ import { FlexLayoutModule } from "@angular/flex-layout";
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSelectModule } from '@angular/material/select';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule} from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatRadioModule } from '@angular/material/radio';
+
+/** OUR COMPONENTS */
 
 import { frontIncomesComponent } from './components/frontincomes/frontincomes.component';
 import { frontExpensesComponent } from './components/frontexpenses/frontexpenses.component';
 import { IncomesComponent } from './components/incomes/incomes.component';
+import { ExpensesComponent } from './components/expenses/expenses.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { SignoutComponent } from './components/signout/signout.component';
