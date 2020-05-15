cc436036fab591f50b911c9bdfab2bed8b524ea5
CoderCris <crisamer.ortega@gmail.com>
2020-05-15 17:05:19 +0200

    balance tunning

--- src/app/components/balance/balance.component.ts
+++ src/app/components/balance/balance.component.ts
@@ -12,19 +12,29 @@ export class BalanceComponent implements OnInit {
   incomeVal: number;
   outcomeVal: number;
 
-  constructor() { }
+  constructor() {
+    this.incomeVal = 0;
+    this.outcomeVal = 0;
+  }
   ngOnInit(): void {}
 
   getBalance() {
     var user = firebase.auth().currentUser;
     if (user) {
-      var incomes = db.collection("usuarios").doc(user.uid).collection("incomes").get().then(function (totalIncomes) {
+
+      var localIncome = db.collection("usuarios").doc(user.uid).collection("incomes").get().then(function (totalIncomes) {
+        var aux = 0;
         totalIncomes.forEach(function (object) {
-          // doc.data() is never undefined for query doc snapshots
-          console.log(object.id, " => ", object.data());
+          //incomeVal +=  (object.data().amount);
+          // object.data() is never undefined for query doc snapshots
+          //console.log(object.id, " => ", object.data().amount);
+          return aux += +object.data().amount;
         });
+        return aux;
       });
+      console.log(localIncome);
       var outcomes = db.collection("usuarios").doc(user.uid).collection("outcomes").get();
+      console.log("income =", this.incomeVal);
     }
   }
 }
