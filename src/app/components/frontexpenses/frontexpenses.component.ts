142e86774c05e3487e5576a7e885eb2b3a4cc32e
Viviana Benitez <magi61012@gmail.com>
2020-05-25 14:19:58 +0100

    doned show datas ingress and expenses from frontpage

--- src/app/components/frontexpenses/frontexpenses.component.ts
+++ src/app/components/frontexpenses/frontexpenses.component.ts
@@ -1,22 +1,8 @@
-import  {Component, OnInit} from '@angular/core';
-import { db } from 'src/app/services/utils/firebase';
+import  {Component} from '@angular/core';
 import { UserService } from '../../services/userService';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AngularFirestore } from '@angular/fire/firestore';
-import * as firebase from 'firebase';
-
-import {MatPaginator} from '@angular/material/paginator';
-import {MatSort} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
-// import { AngularFireDatabase } from 'angularfire2/database';
-
-export interface Expense {
-    category: string;
-    name_expense: string;
-    date: string;
-    amount:number;
-  }
 
 
 @Component({
@@ -25,60 +11,17 @@ export interface Expense {
     styleUrls: ['./frontexpenses.component.css'],
 
 })
-export class frontExpensesComponent implements OnInit{
+export class frontExpensesComponent {
 
-    displayedColumns: string[] = ['name', 'amount'];
-    dataSource: MatTableDataSource<Expense>;
     public items: Observable<any[]>;
-    public gastos: Array<Expense> = [];
-    // amount:string;
-    // category:string;
-    // date:string;
-    // name:string;
 
     constructor(private userService: UserService,private router: Router,firestore: AngularFirestore) {
-        
-        // firebase.auth().onAuthStateChanged(function(user) {
-        //     if (user) {
-        //         // this.items = firestore.collection('usuarios').doc(user.uid).collection("expenses").valueChanges().subscribe(amounts => {amounts.forEach(exp => {this.gastos.push(exp);})
-        //         //     this.dataSource = new MatTableDataSource(this.gastos);
-        //         // });
-        //         this.items=db.collection('usuarios').doc(user.uid).collection("expenses").onSnapshot;
-        //         this.items.subscribe(expenses => {expenses.forEach(gasto => {
-        //               this.gastos.push(gasto);
-        //             })
-        //             this.dataSource = new MatTableDataSource(this.gastos);
-        //         })
-        //         this.dataSource = new MatTableDataSource(this.gastos);
-        //     }
-        // })
-        
+        var user = this.userService.getCurrentUser();
+        if (user) {
+            this.items = firestore.collection('usuarios').doc(user.uid).collection("expenses").valueChanges();
+        }
     }
 
-    ngOnInit(): void {
-        
-        // firebase.auth().onAuthStateChanged(function(user) {
-        //     if (user) {
-        //         db.collection("usuarios").doc(user.uid).collection("expenses").get().then(function(querySnapshot) {
-        //             querySnapshot.forEach(function(doc) {
-                        
-                        
-        //                 // return doc.data().amount;
-        //                 // this.docs.push(doc.data());
-                      
-        //                 // this.amounts.push(doc.data().amount);
-        //                 // console.log(this.amounts);
-        //                 //console.log(doc.id, " => ", doc.data());
-        //             });
-       
-        //         });
-              
-        //     }else{
-        //             console.log("No hay usuario")
-        //     }
-        // })
-     
-    }
 
     addNewExpense(){
         this.router.navigate(['/expenses']);
--- src/app/components/frontincomes/frontincomes.component.ts
+++ src/app/components/frontincomes/frontincomes.component.ts
@@ -1,13 +1,8 @@
-import  {Component, OnInit} from '@angular/core';
-import { db } from 'src/app/services/utils/firebase';
+import  {Component} from '@angular/core';
 import { UserService } from '../../services/userService';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AngularFirestore } from '@angular/fire/firestore';
-import * as firebase from 'firebase';
-import { User, firestore } from 'firebase';
-
-
 
 
 @Component({
@@ -17,45 +12,12 @@ import { User, firestore } from 'firebase';
 })
 export class frontIncomesComponent {
     items: Observable<any[]>;
-    // private data;
-    amount:string;
-    category:string;
-    date:string;
-    name:string;
 
     constructor(private userService: UserService,private router: Router,firestore: AngularFirestore) {
         var user = this.userService.getCurrentUser();
         if (user) {
             this.items = firestore.collection('usuarios').doc(user.uid).collection("incomes").valueChanges();
         }
-    }
-
-    ngOnInit(): void {
-        // var user = this.userService.getCurrentUser()
-        // if (user) {
-        //     db.collection("usuarios").doc(user.uid).collection("incomes").get().then(function(querySnapshot) {
-        //         querySnapshot.forEach(function(doc) {
-                    
-        //             console.log(doc.id, " => ", doc.data());
-                // .then((doc) => {
-                //     if(doc.exists){
-                //         // this.data = doc.data()
-                //         // console.log(this.data)
-                //         this.amount = doc.data().amount;
-                //         this.name = doc.data().name_income;
-                //         this.date = doc.data().date;
-                //         console.log(this.amount);
-                //         console.log(this.name);
-                //         console.log(this.date);
-                //     }else{
-                //         console.log("doc no existe")
-                //     }
-            //     })
-            // })
-       // }
-        // }else{
-        //         console.log("No hay usuario")
-        // }
     }
 
     addNewIncomes(){
